47246a3accf3a2273f5768c43144e127becc2056
trphloc <trphloc@gmail.com>
2014-08-16 22:43:25 +0700

    Site updated at 2014-08-16 15:43:25 UTC

---
+++ javascripts/generate-toc.js
@@ -0,0 +1,13 @@
+ function generateTOC(heading) {
+  	   var container = jQuery("<section></section>");
+	   var div = jQuery("<ul id='toc'></ul>");
+	   var content = jQuery('.sidebar');
+
+	   if (heading != undefined && heading != null) {
+	      container.append('<h1 class="tocHeading">' + heading + '</h1>');
+	   }
+
+	   div.tableOfContents("#content",{startLevel: 2});
+	   container.append(div);
+	   content.prepend(container);
+  }
